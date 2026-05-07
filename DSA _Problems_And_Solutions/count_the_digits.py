abdbb8596362be51abb288c1aef39f15e4d62d62
kiranstdy13 <kiranstdy.13@gmail.com>
2026-05-07 23:02:08 +0530

    Update count_the_digits.py

--- DSA _Problems_And_Solutions/count_the_digits.py
+++ DSA _Problems_And_Solutions/count_the_digits.py
@@ -13,3 +13,17 @@ while n > 0:
     n = n // 10
     count += 1
 print("Number of digits:", count)
+
+
+# time complexity O(1) Advance
+
+from math import *
+
+def count_digits(n):
+    if n == 0:
+        return 1
+    return int(log10(n)) + 1
+
+n = int(input("Enter a number: "))
+print("Number of digits:", count_digits(n))
+
