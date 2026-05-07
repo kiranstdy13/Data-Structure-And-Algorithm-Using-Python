18ea83cabb75e357cd2c5be8adb8233775432776
kiranstdy13 <kiranstdy.13@gmail.com>
2026-05-07 22:42:13 +0530

    Add docstring and comments for clarity
    Added a docstring to explain the purpose of the script and its time complexity.

--- DSA _Problems_And_Solutions/extract_the_digiht.py
+++ DSA _Problems_And_Solutions/extract_the_digiht.py
@@ -1,10 +1,18 @@
+'''
+    Extract the digits in a integer number
 
+    input = 345
+    oupt = 5 4 3
+'''
+# time complexity O(log 10(n))
 n = int(input("Enter a number: "))
 
 while n > 0:
     digit = n % 10
     print(digit)
     n = n // 10
+
+# time complexity O(1) Advance
 
 from math import *
 
